ed7ebf60b3ae65d1458e0a7a6165c84e8132113a
adminmttt <mingtlin@foxmail.com>
2023-02-12 15:03:42 +0800

    git no verify

--- src/views/data-managerment/index.vue
+++ src/views/data-managerment/index.vue
@@ -1,8 +1,3 @@
 <template>
-  <CommonPage>
-    <div>a</div>
-    <div pl-20>
-      <RouterView />
-    </div>
-  </CommonPage>
+
 </template>
--- src/views/data-managerment/sql-editor/index.vue
+++ src/views/data-managerment/sql-editor/index.vue
@@ -1,42 +1,90 @@
 <template>
-  <CommonPage>
-    <div h-60 pl-20 pr-20 flex items-center bg-white>
-      <input
-        v-model="post.title"
-        class="flex-1 pt-15 pb-15 mr-20 text-20 font-bold color-primary"
-        type="text"
-        placeholder="输入文章标题..."
-      />
-      <n-button type="primary" style="width: 80px" :loading="btnLoading" @click="handleSavePost">
-        <TheIcon v-if="!btnLoading" icon="line-md:confirm-circle" class="mr-5" :size="18" /> 保存
-      </n-button>
-    </div>
-    <MdEditor v-model="post.content" style="height: calc(100vh - 305px)" />
-  </CommonPage>
+    <CommonPage show-footer>
+        <div class="app-container">
+            <n-card shadow="never" >
+                <div class="editor-tools">
+                    <el-form-item>
+                    <n-button type="primary" @click="handleExecution">解析</n-button>
+                    <n-button @click="resetEditor">重置</n-button>
+                    </el-form-item>
+                </div>
+                <div class="editor-wrapper">
+                    <codemirror
+                    v-model="code"
+                    placeholder="Code goes here..."
+                    :style="{ height: '400px'}"
+                    :autofocus="true"
+                    :indent-with-tab="true"
+                    :tab-size="2"
+                    :extensions="extensions"
+                    @ready="handleReady"
+                    @change="log('change', $event)"
+                    @focus="log('focus', $event)"
+                    @blur="log('blur', $event)"
+                    />
+                </div>
+            </n-card>
+        </div>
+    </CommonPage>
 </template>
 
 <script setup>
 
-import MdEditor from 'md-editor-v3'
-import 'md-editor-v3/lib/style.css'
+import { defineComponent,ref,shallowRef } from 'vue'
+import { Codemirror } from 'vue-codemirror'
+//import { javascript } from '@codemirror/lang-javascript'
+import { sql } from '@codemirror/lang-sql'
+//import { useCodeStore } from '@/store/editor/code'
 
-defineOptions({ name: 'MDEditor' })
+const code = ref(`select * from users;`)
+const extensions = [ sql() ] //javascript() 
 
-// refs
-let post = ref({})
-let btnLoading = ref(false)
-
-function handleSavePost(e) {
-  btnLoading.value = true
-  $message.loading('正在保存...')
-  setTimeout(() => {
-    $message.success('保存成功')
-    btnLoading.value = false
-  }, 2000)
+// Codemirror EditorView instance ref
+const view = shallowRef()
+const handleReady = (payload) => {
+  view.value = payload.view
 }
+
+// Status is available at all times via Codemirror EditorView
+const getCodemirrorStates = () => {
+  const state = view.value.state
+  const ranges = state.selection.ranges
+  const selected = ranges.reduce((r, range) => r + range.to - range.from, 0)
+  const cursor = ranges[0].anchor
+  const length = state.doc.length
+  const lines = state.doc.lines
+  
+}
+
+const log = console.log;
+
+const resetEditor = () => {
+  //code.value = useCodeStore().resetCode();
+  //"select * from users;"
+  console.log('resetEditor')
+  
+}
+
+const handleExecution = () =>{
+  console.log('handleExecution')
+  //useCodeStore().parseCode(code.value);
+}
+
 
 </script>
 
 <style lang="scss" scoped>
+
+
+.editor-tools {
+  margin-bottom: 20px;
+  :deep(.el-card__body) {
+    padding-bottom: 2px;
+  }
+}
+
+.editor-wrapper {
+  margin-bottom: 5px;
+}
 
 </style>
